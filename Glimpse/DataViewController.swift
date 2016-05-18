1d5df7053336e981c762bce286ca405f41f9419b
Utkarsh <theutkarsh246@gmail.com>
2016-05-18 13:48:56 -0400

    # Start

--- Glimpse/DataViewController.swift
+++ Glimpse/DataViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class DataViewController: UIViewController {
 
-    @IBOutlet weak var dataLabel: UILabel!
     var dataObject: String = ""
 
 
@@ -19,14 +18,9 @@ class DataViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        self.dataLabel!.text = dataObject
+        
     }
 
 
